3a239e5efd131d04e06b90204185990afbface83
kanishka-linux <kanishka.linux@gmail.com>
2019-01-17 14:41:38 +0530

    handle case when key is type

--- lib/jake/map_util.ex
+++ lib/jake/map_util.ex
@@ -1,3 +1,6 @@
+# Ref: https://stackoverflow.com/questions/38864001/elixir-how-to-deep-merge-maps
+# Ref: https://github.com/activesphere/jake/blob/master/lib/jake/map_util.ex
+
 defmodule Jake.MapUtil do
   def deep_merge(left, right) do
     Map.merge(left, right, &deep_resolve/3)
@@ -9,6 +12,15 @@ defmodule Jake.MapUtil do
 
   defp deep_resolve(_key, nil, right) do
     right
+  end
+
+  defp deep_resolve(key, left, right) when key == "type" do
+    case {is_list(left), is_list(right)} do
+      {x, y} when x and y -> left ++ right
+      {x, y} when x and not y -> left ++ [right]
+      {x, y} when not x and y -> [left] ++ right
+      {x, y} when not x and not y -> [left, right]
+    end
   end
 
   defp deep_resolve(_key, left, right) when is_map(left) do
